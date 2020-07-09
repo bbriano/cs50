087e65a43e97166198f4f7e5ac2d36d4f0d531b4
Briano Goestiawan <bgoestiawan@gmail.com>
2020-07-10 02:37:21 +1000

    Add update and delete functionality

--- 8-sql/app.py
+++ 8-sql/app.py
@@ -5,110 +5,57 @@ import sqlite3
 app = Flask(__name__)
 
 
-def get_all_tasks():
+def render_all_tasks():
     conn = sqlite3.connect("app.db")
     c = conn.cursor()
     c.execute("SELECT * FROM tasks")
     tasks = c.fetchall()
     conn.close()
-    return tasks
+    return render_template("index.j2", tasks=tasks)
 
 
-def add_task(text):
+@app.route("/")
+def get():
+    """Show all tasks"""
+    return render_all_tasks()
+
+
+@app.route("/", methods=["POST"])
+def post():
+    """Create task then redirect home"""
+    text = request.form.get("text")
     conn = sqlite3.connect("app.db")
     c = conn.cursor()
     c.execute("INSERT INTO tasks (text) VALUES (?)", (text,))
     conn.commit()
     conn.close()
-
-
-def complete_task(id):
-    conn = sqlite3.connect("app.db")
-    c = conn.cursor()
-    c.execute("UPDATE INTO tasks VALUES (?)", (text,))
-    conn.commit()
-    conn.close()
-
-
-@app.route("/", methods=["GET"])
-def get():
-    """
-    IF session not exist, redirect to signin page
-    ELSE show home page with user tasks
-    """
-    tasks = get_all_tasks()
-    return render_template("index.html", tasks=tasks)
-
-
-@app.route("/", methods=["POST"])
-def post():
-    """
-    IF session not exist, redirect to signin page
-    ELSE create task then redirect to home page
-    """
-    text = request.form.get("text")
-    add_task(text)
-    return redirect("/")
+    return render_all_tasks()
 
 
 @app.route("/", methods=["PUT"])
 def put():
-    """
-    IF session not exist, redirect to signin page
-    ELSE update todo then send user tasks
-    """
-    pass
+    """Update task then redirect home"""
+    id = request.json['id']
+    done = request.json['done']
+    conn = sqlite3.connect("app.db")
+    c = conn.cursor()
+    c.execute("UPDATE tasks SET done=? WHERE id=?", (done, id))
+    conn.commit()
+    conn.close()
+    return render_all_tasks()
 
 
 @app.route("/", methods=["DELETE"])
 def delete():
-    """
-    IF session not exist, redirect to signin page
-    ELSE delete task then send user tasks
-    """
-    pass
+    """Delete task then redirect home"""
+    id = request.json['id']
+    conn = sqlite3.connect("app.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM tasks WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return render_all_tasks()
 
 
-@app.route("/signup", methods=["GET"])
-def get_signup():
-    """
-    IF session exist, redirect to home page
-    ELSE show signup page
-    """
-    pass
-
-
-@app.route("/signup", methods=["POST"])
-def post_signup():
-    """
-    IF session exist, redirect to home page
-    ELSE create new user then redirect to home page
-    """
-    pass
-
-
-@app.route("/signin", methods=["GET"])
-def get_signin():
-    """
-    IF session exist, redirect to home page
-    ELSE create session then redirect to home page
-    """
-    pass
-
-
-@app.route("/signin", methods=["POST"])
-def post_signin():
-    """
-    IF session exist, redirect to home page
-    ELSE create user session then redirect to home page
-    """
-    pass
-
-
-@app.route("/signout", methods=["GET"])
-def get_signout():
-    """
-    IF session not exist, redirect to signin page
-    ELSE kill session then redirect to signin page
-    """
-    pass
+if __name__ == "__main__":
+    app.run(debug=True)
